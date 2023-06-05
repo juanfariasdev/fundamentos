1e77f94ccc17c4c8261cddbc40278167f9622dda
Juan Pablo Farias <juanpablo.profissional.a.f@gmail.com>
2023-06-05 16:29:32 -0300

    implement persist DB

--- src/database.js
+++ src/database.js
@@ -1,5 +1,23 @@
+import fs from "node:fs/promises";
+
+const dbname = "../db.json";
+const databasePath = new URL(dbname, import.meta.url);
 export class Database {
   #database = {};
+
+  constructor() {
+    fs.readFile(databasePath, "utf8")
+      .then((file) => {
+        this.#database = JSON.parse(file);
+      })
+      .catch(() => {
+        this.#persist();
+      });
+  }
+
+  #persist() {
+    fs.writeFile(databasePath, JSON.stringify(this.#database));
+  }
 
   select(table) {
     const data = this.#database[table] ?? [];
@@ -13,6 +31,8 @@ export class Database {
     } else {
       this.#database[table] = [data];
     }
+
+    this.#persist();
 
     return data;
   }
